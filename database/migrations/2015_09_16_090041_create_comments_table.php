0606cc47428fd7bd9bab13913f8ab1282718a171
artsoroka <githubrepository@yandex.ru>
2015-09-16 11:44:37 +0000

    created Comment model with migration

---
+++ database/migrations/2015_09_16_090041_create_comments_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned(); 
+            $table->integer('user_id')->unsigned(); 
+            $table->text('text'); 
+            $table->timestamps();
+        });
+        
+        Schema::table('comments', function($table) { 
+            $table->foreign('post_id')->references('id')->on('posts'); 
+            $table->foreign('user_id')->references('id')->on('users'); 
+        }); 
+        
+        Schema::create('post_comment', function($table) { 
+            $table->integer('post_id')->unsigned(); 
+            $table->integer('comment_id')->unsigned(); 
+            $table->timestamps();
+        }); 
+        
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+        Schema::drop('post_comment'); 
+    }
+}
